4c756b5803fa607b0cd67bd97f88f3919a26ae3e
Siekierson <kisiel123.kisiel@gmail.com>
2020-05-07 15:01:20 +0200

    Updates

---
+++ precache-manifest.136135eb106a7852ccba0655818d3118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e362d6d5b24b01ea0c94840a32bff4fb",
+    "url": "/ChartMakerApp/index.html"
+  },
+  {
+    "revision": "5893560484297fcdea16",
+    "url": "/ChartMakerApp/static/css/main.e8c6fd7c.chunk.css"
+  },
+  {
+    "revision": "84af1c4134367f7c34cf",
+    "url": "/ChartMakerApp/static/js/2.71dcd923.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/ChartMakerApp/static/js/2.71dcd923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5893560484297fcdea16",
+    "url": "/ChartMakerApp/static/js/main.375ef7cd.chunk.js"
+  },
+  {
+    "revision": "fb353529f0ff1be8bac5",
+    "url": "/ChartMakerApp/static/js/runtime-main.f300b625.js"
+  }
+]);
