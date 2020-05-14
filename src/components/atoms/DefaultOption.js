d8e185f4264ada16d107ef98a9b8556025b95036
Siekierson <kisiel123.kisiel@gmail.com>
2020-05-14 19:43:28 +0200

    trying repair gh pages

--- src/components/atoms/DefaultOption.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-const Option = styled.option`
-padding:10px;
-`
-const DefaultOption=({inner})=> <Option>{inner}</Option>
-export default DefaultOption
